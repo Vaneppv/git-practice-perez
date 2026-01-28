bb62354b8d96a8b7d3672c6d820149f46fb9c3ba
Vanessa Perez <vaneppv@gmail.com>
2026-01-27 20:56:37 -0400

    feat: agregar más mensajes al programa

--- git-practice-perez/hola-mundo.cpp
+++ git-practice-perez/hola-mundo.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int main() {
     cout << "¡Hola Mundo desde Git!" << endl;
-    cout << "Mi nombre es: [Tu Nombre]" << endl;
+    cout << "Mi nombre es: Vanessa" << endl;
+    cout << "Estoy aprendiendo Git en Programación II" << endl;
+    cout << "Este es mi segundo commit!" << endl;
+
     return 0;
 }
